4c2a3e5a4627e58984d043df91baf4a44ca89c29
DarkPrometheus <emanuel.frias17@tectijuana.edu.mx>
2021-01-06 09:35:00 -0800

    Boton para copiar gif

--- gif-expert-app/src/components/GifGridItem.jsx
+++ gif-expert-app/src/components/GifGridItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-export const GifGridItem = ({title, url}) => {
+export const GifGridItem = ({ title, url }) => {
     return (
         <div className="card animate__animated animate__bounceInLeft">
             <img src={url} alt={title} />
             <p>{title}</p>
+            <CopyToClipboard text={url}>
+                <i class="fas fa-copy"></i>
+            </CopyToClipboard>
         </div>
     )
 }
